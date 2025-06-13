f8481699d8ba23d50ffafa0754f737055bc91e29
Shayan-Mst <shnbackup007@gmail.com>
2025-06-13 22:14:56 +0330

    added useMemo to filter the required data to show

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -10,7 +10,15 @@ export default function MapView({center}) {
  
   const { data, error , isLoading } = useSWR("/data/data.json");
 
- 
+ const points = useMemo(() => {
+    
+    if (!data) return [];
+   
+   return  data.slice(20,30);
+   
+     
+  }, [data]);
+
   
   if (isLoading) return <Spinner/>;
   if (error) return <p>خطا در دریافت داده‌ها</p>;
@@ -20,7 +28,7 @@ export default function MapView({center}) {
     <div id="map" style={{ height: '90vh', width: '100%' }}>
       <MapContainer center={{ lat: 35.7006, lng: 51.3377 }} zoom={13} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {data.map(point => (
+        {points.map(point => (
           <Marker key={point.id} position={[point.lat, point.lng]} >
             <Tooltip>{point.name}</Tooltip>
           </Marker>
