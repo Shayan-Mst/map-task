e214e1675eda9b5344be96dca8c6678ce5a9aa30
Shayan-Mst <shnbackup007@gmail.com>
2025-06-13 22:28:28 +0330

    wrapped app in SWRProvider to benefit entire app from using swr

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'leaflet/dist/leaflet.css';
+import { SWRProvider } from './config/swrConfig.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SWRProvider>
     <App />
+    </SWRProvider>
   </StrictMode>,
 )
